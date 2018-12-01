1ce79863f5605a7a6db0135dd13f9c4b11cad141
Jeremy Vineyard <coinbump@gmail.com>
2018-12-01 11:47:09 -0800

    Add basic paths support to Node2D
    To use this:
    1. Create an empty game object, create child objects, attach MovePath2D to the parent game object, set up
    2. Attach Node2D to your sprite, set the path info (link it to the game parent game object that defines the path)

---
+++ Phase Jump/Assets/phasejumppro/Core/MovePath2D.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePath2D : MonoBehaviour
+{
+
+	private enum RenderState
+	{
+		Default,
+		Selected
+	}
+
+	public bool isLooping = false;
+	public List<GameObject> waypoints = new List<GameObject>();
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnValidate()
+	{
+	}
+
+	public List<Vector3> GetWaypointPositions()
+	{
+		if (waypoints.Count == 0) { return new List<Vector3>(); }
+
+		List<Vector3> positions = new List<Vector3>();
+
+		foreach (GameObject waypoint in waypoints)
+		{
+			positions.Add(waypoint.transform.position);
+		}
+		if (isLooping)
+		{
+			positions.Add(waypoints[0].transform.position);
+		}
+
+		return positions;
+	}
+
+	public List<Vector2> GetWaypointPositions2D()
+	{
+		List<Vector3> result3D = GetWaypointPositions();
+		List<Vector2> result = new List<Vector2>();
+
+		foreach (Vector3 v in result3D)
+		{
+			result.Add(v);
+		}
+
+		return result;
+	}
+
+	private void RenderGizmos(RenderState renderState)
+	{
+		if (waypoints.Count == 0) { return; }
+
+		List<Vector3> positions = GetWaypointPositions();
+
+		bool hasFirstPosition = false;
+		Vector3 prevPosition = new Vector3();
+		Vector3 positionStart, positionEnd;
+
+		foreach (Vector3 position in positions)
+		{
+			positionStart = prevPosition;
+			positionEnd = position;
+
+			if (hasFirstPosition)
+			{
+				switch (renderState)
+				{
+					case RenderState.Default:
+						Gizmos.color = Color.white;
+						break;
+					case RenderState.Selected:
+						Gizmos.color = GUI.skin.settings.selectionColor;
+						break;
+				}
+				Gizmos.DrawLine(positionStart, positionEnd);
+			}
+			hasFirstPosition = true;
+			prevPosition = position;
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		RenderGizmos(RenderState.Default);
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		RenderGizmos(RenderState.Selected);
+	}
+}
--- Phase Jump/Assets/phasejumppro/Core/Node2D.cs
+++ Phase Jump/Assets/phasejumppro/Core/Node2D.cs
@@ -16,12 +16,29 @@ namespace PJ
 	public class Node2D : MonoBehaviour
 	{
 		private float _rotation;    // Simplifies basic 2D rotation
-		public bool dontModRotation = false;
 		public bool isKinematic;
 
+		// Paths:
+		[System.Serializable]
+		public class PathInfo
+		{
+			public GameObject path;
+			public float speed = 1.0f;
+			public int waypointOrigin;
+
+			[UnityEngine.Range(0, 1.0f)]
+			public float waypointProgress;
+
+			public bool isMovingForward = true;
+		}
+		public PathInfo pathInfo = new PathInfo();
+
+		[Space(10)]
 		// Kinematic motion:
 		public float directionVelocity;
 		public Vector2 velocity;
+
+		public bool dontModRotation = false;
 
 		// Normalized rotation value (0-1.0)
 		public float RotationNormal
@@ -65,17 +82,154 @@ namespace PJ
 		private void OnValidate()
 		{
 			transform.eulerAngles = new Vector3(0, 0, -360.0f * _rotation);
+
+			if (null == pathInfo.path) { return; }
+			MovePath2D movePath = pathInfo.path.GetComponent("MovePath2D") as MovePath2D;
+			if (null == movePath) { return; }
+
+			pathInfo.waypointOrigin = Math.Min(pathInfo.waypointOrigin, movePath.waypoints.Count - 1);
+			pathInfo.waypointOrigin = Math.Max(pathInfo.waypointOrigin, 0);
+			SnapToPath();
+		}
+
+		// Update is called once per frame
+		protected virtual void Update()
+		{
+			UpdateNode();
+		}
+
+		protected void GetPathPositions(MovePath2D movePath, out Vector2 positionStart, out Vector2 positionEnd)
+		{
+			int origin = pathInfo.waypointOrigin;
+			var positions = movePath.GetWaypointPositions2D();
+
+			positionStart = transform.position;
+			positionEnd = transform.position;
+
+			if (positions.Count < 2) { return; }
+
+			positionStart = positions[origin];
+			positionEnd = positionStart;
+
+			if (pathInfo.isMovingForward)
+			{
+				positionEnd = positions[origin + 1];
+			}
+			else
+			{
+				if (origin > 0)
+				{
+					positionEnd = positions[origin - 1];
+				}
+				else if (movePath.isLooping)
+				{
+					positionEnd = positions[positions.Count - 2];
+				}
+			}
+		}
+
+		protected void SnapToPath()
+		{
+			if (null == pathInfo.path) { return; }
+			MovePath2D movePath = pathInfo.path.GetComponent("MovePath2D") as MovePath2D;
+			if (null == movePath)
+			{
+				Debug.Log("Warning. Path is missing MovePath2D component"); return;
+			}
+
+			int origin = pathInfo.waypointOrigin;
+			var positions = movePath.GetWaypointPositions2D();
+
+			if (positions.Count < 2) { return; }
+
+			Vector2 positionStart;
+			Vector2 positionEnd;
+			GetPathPositions(movePath, out positionStart, out positionEnd);
+
+			Vector2 position = Vector2.Lerp(positionStart, positionEnd, pathInfo.waypointProgress);
+
+			transform.position = position;
+		}
+
+		/// <summary>
+		/// On update, moves the object along a MovePath2D.
+		/// 
+		/// FUTURE: add interpolation support if needed.
+		/// FUTURE: support spline paths/curves
+		/// 
+		/// </summary>
+		protected void UpdateFollowPath()
+		{
+			if (null == pathInfo.path) { return; }
+			MovePath2D movePath = pathInfo.path.GetComponent("MovePath2D") as MovePath2D;
+			if (null == movePath) {
+				Debug.Log("Warning. Path is missing MovePath2D component"); return; 
+			}
+
+			var positions = movePath.GetWaypointPositions2D();
+
+			// TODO: this only works with simple paths, we need better logic here.
+			pathInfo.waypointProgress += Time.deltaTime*pathInfo.speed;
+
+			while (pathInfo.waypointProgress >= 1.0f)
+			{
+				pathInfo.waypointProgress -= 1.0f;
+
+				if (pathInfo.isMovingForward) {
+					pathInfo.waypointOrigin += 1;
+
+					if (movePath.isLooping)
+					{
+						if (pathInfo.waypointOrigin >= positions.Count - 1) {
+							pathInfo.waypointOrigin = 0;
+						}
+					}
+					else
+					{
+						if (pathInfo.waypointOrigin >= positions.Count - 1)
+						{
+							pathInfo.waypointOrigin = movePath.waypoints.Count - 1;
+							pathInfo.isMovingForward = false;
+						}
+					}
+				}
+				else
+				{
+					pathInfo.waypointOrigin -= 1;
+					if (pathInfo.waypointOrigin < 0 && movePath.isLooping)
+					{
+						pathInfo.waypointOrigin = movePath.waypoints.Count - 1;
+					}
+					else if (pathInfo.waypointOrigin <= 0 && !movePath.isLooping)
+					{
+						pathInfo.waypointOrigin = 0;
+						pathInfo.isMovingForward = true;
+					}
+				}
+			}
+
+			SnapToPath();
 		}
 
 		protected void UpdateNode()
 		{
 			if (!isKinematic) { return; }
+
+			// MOVEMENT TYPE: Follow Path (requires MovePath2D component on Path object)
+			if (pathInfo.path != null)
+			{
+				UpdateFollowPath();
+				return;
+			}
+
+			// MOVEMENT TYPE: Directional Velocity (for objects that move where they're pointed)
 			var velocityVector = velocity;
 			if (!directionVelocity.Equals(0)) {
 				var radians = RotationAngle * 0.01745329252f;
 				velocityVector = new Vector2((float)Math.Sin(radians)* directionVelocity, (float)Math.Cos(radians) * directionVelocity);
 			}
 
+			// MOVEMENT TYPE: Kinematic velocity (for objects that don't require physics)
 			var position = transform.position;
 			position.x += velocityVector.x * Time.deltaTime;
 			position.y += velocityVector.y * Time.deltaTime;
