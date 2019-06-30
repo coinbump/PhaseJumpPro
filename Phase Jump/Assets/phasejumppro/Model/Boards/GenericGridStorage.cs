23153aacad860faeba258bebd8eda9d320c72f3b
Jeremy Vineyard <coinbump@gmail.com>
2019-06-30 16:15:02 -0700

    More GenericGridStorage cleanup (WIP)
    GenericGridStorage

--- Phase Jump/Assets/phasejumppro/Model/Boards/GenericGridStorage.cs
+++ Phase Jump/Assets/phasejumppro/Model/Boards/GenericGridStorage.cs
@@ -8,7 +8,7 @@ namespace PJ
 	/// Stores objects in an XxY grid structure (Y rows of X cells)
 	/// </summary>
 	/// <typeparam name="T">Type of thing to store</typeparam>
-	public class GenericGridStorage<T>
+	public class GenericGridStorage<T> where T : new()
 	{
 		#region Types
 		public class Row : List<T>
@@ -42,6 +42,7 @@ namespace PJ
 		}
 		#endregion
 
+		#region Initializers
 		public GenericGridStorage()
 		{
 		}
@@ -62,140 +63,106 @@ namespace PJ
 				rows.Add(row);
 			}
 		}
+		#endregion
+
+		Row GetRow(int index) { return index < rows.Count ? rows[index] : null; }
+
+		void Resize(Vector2Int newSize)
+		{
+			var width = newSize.x;
+			var height = newSize.y;
+
+			if (width < 1 || height < 1)
+			{
+				Debug.LogError(string.Format("Invalid grid storage size {0}, {1}.", size.x, size.y));
+				return;
+			}
+
+			// Same size.
+			if (width == Width && height == Height) { return; }
+
+			//	// Change width of existing rows first.
+			//	int y = 0;
+			//	for (typename PJ_TStdVector< Row *>.iterator i = rows.begin(); i != rows.end(); i++, y++) {
+
+			//		// Don't bother resizing rows that are going to be deleted if the
+			//		// grid is shrinking.
+			//		if (y >= height)
+			//		{
+			//			break;
+			//		}
+
+			//		Row row = (*i);
+			//		int sizeX = Width;
+
+			//		while (sizeX < width)
+			//		{
+			//			row->Add(defaultVal);
+			//			sizeX++;
+			//		}
+			//		if (sizeX > width)
+			//		{
+			//			int diff = sizeX - width;
+
+			//			typename Row.iterator  first = row->end();
+			//			first -= diff;
+			//			removeCells(row, first, row->end());
+
+			//			sizeX = width;
+			//		}
+			//	}
+
+			//	if (Height > height)
+			//	{
+			//		int diff = Height - height;
+
+			//		typename PJ_TStdVector<Row*>.iterator  first = rows.end() - diff;
+			//		RemoveRows(first, rows.end());
+			//		Height = height;
+			//	}
+			//	else
+			//	{
+			//		while (Height < height)
+			//		{
+			//			Row row = new Row(width, defaultVal);
+			//			rows.Add(row);
+			//			Height++;
+			//		}
+			//	}
+
+			//	Width = width;
+			//	Height = height;
+
+		}
+
+		public bool IsValidLoc(Vector2Int loc) {
+			return (loc.x >= 0 && loc.x < Width &&
+					loc.y >= 0 && loc.y < Height);
+		}
+
+		public virtual T GetCell(Vector2Int loc)
+		{
+			if (loc.x >= 0 && loc.x < Width &&
+				loc.y >= 0 && loc.y < Height)
+			{
+				Row row = rows[loc.y];
+				return (row)[loc.x];
+			}
+
+			return new T();
+		}
+
+		public virtual void SetCell(Vector2Int loc, T content)
+		{
+			if (loc.x >= 0 && loc.x < Width &&
+				loc.y >= 0 && loc.y < Height)
+			{
+				Row row = GetRow(loc.y);
+				if (null != row)
+				{
+					(row)[loc.x] = content;
+				}
+			}
+		}
 	}
 }
-
-// TODO: port this
-
-//protected:
-//	virtual void removeCells(Row row, CellIterator first, CellIterator end)
-//{
-//	row->erase(first, end);
-//}
-
-//virtual void RemoveRows(RowIterator first, RowIterator end)
-//{
-
-//	for (typename PJ_TStdVector< Row *>.iterator i = first; i != end; i++) {
-//		Row row = (*i);
-//		delete row;
-
-//	}
-
-//	rows.erase(first, end);
-
-//}
-
-//void Resize(int width, int height, T defaultVal)
-//{
-
-//# ifdef __DEBUG__
-//	if (width < 1 || height < 1)
-//	{
-//		PJLog("ERROR. Invalid grid size %d, %d", width, height);
-//		return;
-//	}
-//#endif
-
-//	// Same size.
-//	if (width == mSize[0] && height == mSize[1]) { return; }
-
-//	// Change width of existing rows first.
-//	int y = 0;
-//	for (typename PJ_TStdVector< Row *>.iterator i = rows.begin(); i != rows.end(); i++, y++) {
-
-//		// Don't bother resizing rows that are going to be deleted if the
-//		// grid is shrinking.
-//		if (y >= height)
-//		{
-//			break;
-//		}
-
-//		Row row = (*i);
-//		int sizeX = mSize[0];
-
-//		while (sizeX < width)
-//		{
-//			row->Add(defaultVal);
-//			sizeX++;
-//		}
-//		if (sizeX > width)
-//		{
-//			int diff = sizeX - width;
-
-//			typename Row.iterator  first = row->end();
-//			first -= diff;
-//			removeCells(row, first, row->end());
-
-//			sizeX = width;
-//		}
-//	}
-
-//	if (mSize[1] > height)
-//	{
-//		int diff = mSize[1] - height;
-
-//		typename PJ_TStdVector<Row*>.iterator  first = rows.end() - diff;
-//		RemoveRows(first, rows.end());
-//		mSize[1] = height;
-//	}
-//	else
-//	{
-//		while (mSize[1] < height)
-//		{
-//			Row row = new Row(width, defaultVal);
-//			rows.Add(row);
-//			mSize[1]++;
-//		}
-//	}
-
-//	mSize[0] = width;
-//	mSize[1] = height;
-
-//}
-
-//inline Row GetRow(typename PJ_TStdVector<Row*>.size_type y) const { return y<rows.size()? rows[y] : NULL; }
-	
-//	bool IsValidLoc(PJ_GridLoc const& loc) const {
-//		return (loc.x >= 0 && loc.x<mSize[0] &&
-//				loc.y >= 0 && loc.y<mSize[1]);
-//	}
-	
-//	virtual T GetCell(PJ_GridLoc loc) const {
-//		if (loc.x >= 0 && loc.x<mSize[0] &&
-//			loc.y >= 0 && loc.y<mSize[1]) {
-			
-//			Row row = rows[loc.y];
-//			return ( row)[loc.x];
-//		}
-		
-//		// http://stackoverflow.com/questions/936999/what-is-the-default-constructor-for-c-pointer
-//		return T();
-		
-//	}
-	
-//	virtual void SetCell(PJ_GridLoc loc, T& content)
-//{
-//	if (loc.x >= 0 && loc.x < mSize[0] &&
-//		loc.y >= 0 && loc.y < mSize[1])
-//	{
-
-//		Row row = GetRow(loc.y);
-//		if (NULL != row)
-//		{
-//			(*row)[loc.x] = content;
-//		}
-//	}
-//}
-
-//typename Row.iterator IteratorOf(PJ_GridLoc const& loc)
-//{
-//	if (loc.x >= mSize[0] || loc.x < 0) { return rows.end(); }
-//	if (loc.y >= mSize[1] || loc.y < 0) { return rows.end(); }
-
-//	Row row = rows[loc.y];
-//	return row->begin() + loc.x;
-
-//}
-	
-//};
