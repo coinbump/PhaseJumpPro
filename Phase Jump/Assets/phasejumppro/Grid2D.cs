3f0e20444eb43293d9ab41317d7a88b73bf31e2e
Jeremy Vineyard <coinbump@gmail.com>
2019-07-07 22:03:02 -0700

    Grid2D WIP

--- Phase Jump/Assets/phasejumppro/Grid2D.cs
+++ Phase Jump/Assets/phasejumppro/Grid2D.cs
@@ -7,9 +7,8 @@ namespace PJ
 {
 	public class GridLoc : MonoBehaviour
 	{
-		public float x;
-		public float y;
-		public float z;
+		public Vector3Int value;
+		public Vector3 offset;
 	}
 
 	/// <summary>
@@ -18,31 +17,85 @@ namespace PJ
 	/// </summary>
 	public class Grid2D : MonoBehaviour
 	{
-		// Start is called before the first frame update
+		public Vector2Int size = new Vector2Int(1, 1);
+		public Vector2 cellSize = new Vector2Int(1, 1);
+		public Vector2 cellGap;
+
 		void Start()
 		{
 
 		}
 
-		// Update is called once per frame
 		void Update()
 		{
 
 		}
 
-		void OnTransformChildrenChanged()
+		Rect CellBounds(GridLoc loc)
+		{
+			var result = new Rect();
+
+			result.xMin = loc.value.x * cellSize.x;
+			if (loc.value.x > 0)
+			{
+				result.xMin += (loc.value.x - 1) * cellGap.x;
+			}
+			result.xMax = loc.value.y * cellSize.y;
+			if (loc.value.y > 0)
+			{
+				result.yMin += (loc.value.y - 1) * cellGap.y;
+			}
+
+			result.size = cellSize;
+
+			// TODO: are we using Cartesian or reading coordinates?
+
+			return result;
+		}
+
+		private GridLoc GridLocFor(GameObject go)
+		{
+			var gridLoc = go.GetComponent<GridLoc>();
+			if (null == gridLoc)
+			{
+				gridLoc = go.AddComponent<GridLoc>();
+
+				// TODO: find the nearest empty grid loc and assign it to the object <- what if there is no empty cell?
+			}
+			return gridLoc;
+		}
+
+		/// <summary>
+		/// For each child object, make sure it has a grid loc, or create one.
+		/// </summary>
+		private void ForceChildLocs()
 		{
 			foreach (Transform child in transform)
 			{
 				var go = child.gameObject;
-				var gridLoc = go.GetComponent<GridLoc>();
-				if (null == gridLoc)
-				{
-					gridLoc = go.AddComponent<GridLoc>();
-				}
-
-
+				var gridLoc = GridLocFor(go);
 			}
+		}
+
+		void OnTransformChildrenChanged()
+		{
+			ResizeChildren();
+		}
+
+		private void ResizeChildren()
+		{
+			foreach (Transform child in transform)
+			{
+				var go = child.gameObject;
+				var loc = GridLocFor(go);
+				var bounds = CellBounds(loc);
+				go.transform.position = bounds.center;	// TODO: support offset
+			}
+		}
+
+		private void OnValidate()
+		{
+			ResizeChildren();
 		}
 	}
 }
