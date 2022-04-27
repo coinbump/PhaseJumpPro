949e98c89888d7d69f8fae067e4d7c187eeb9b87
Jeremy Vineyard <coinbump@gmail.com>
2022-04-26 23:55:05 -0700

    Better Breakout Example logic (WIP)

--- PhaseJumpPro/Assets/Examples/Breakout Example/BKEBall.cs
+++ PhaseJumpPro/Assets/Examples/Breakout Example/BKEBall.cs
@@ -3,7 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using PJ;
 
+/// <summary>
+/// A breakout ball that removes blocks that it hits and reflects when hitting blocks
+/// </summary>
 public class BKEBall : Node2D
 {
+    protected void OnCollisionEnter2D(Collision2D collision)
+    {
+        var myCollider = GetComponent<CircleCollider2D>();
+        var colliderList = new List<Collider2D>();
+        myCollider.OverlapCollider(new ContactFilter2D().NoFilter(), colliderList);
 
+        var isFirst = true;
+        foreach (Collider2D collider in colliderList)
+        {
+            var node = collider.gameObject.GetComponent<Node2D>();
+            if (null == node) { continue; }
+
+            if (!node.HasTypeTag("block")) { continue; }
+
+            if (isFirst)
+            {
+                isFirst = false;
+
+                var contacts = new List<ContactPoint2D>();
+                collider.GetContacts(contacts);
+                Velocity = Vector2.Reflect(Velocity, contacts[0].normal);
+            }
+
+            Destroy(collider.gameObject);
+        }
+    }
 }
--- PhaseJumpPro/Assets/Examples/Breakout Example/BKEPaddle.cs
+++ PhaseJumpPro/Assets/Examples/Breakout Example/BKEPaddle.cs
@@ -5,4 +5,23 @@ using PJ;
 
 public class BKEPaddle : Node2D
 {
+    protected void OnCollisionEnter2D(Collision2D collision)
+    {
+        var node = collision.collider.gameObject.GetComponent<Node2D>();
+        if (null == node) { return; }
+
+        if (!node.HasTypeTag("ball")) { return; }
+
+        Debug.Log("Ball hit paddle");
+
+        ContactPoint2D contactPoint = collision.contacts[0];
+
+        var myCollider = GetComponent<CapsuleCollider2D>();
+        var factor = (contactPoint.point.x - myCollider.bounds.center.x) / (myCollider.bounds.size.x / 2.0f);
+
+        var reflectDegreeAngle = factor * 45.0f;
+        Debug.Log("Paddle factor: " + factor.ToString() + "Reflect angle: " + reflectDegreeAngle);
+
+        node.Velocity = AngleUtils.DegreeAngleToVector2(reflectDegreeAngle, 10);
+    }
 }
