adfe1167c55f49889fdfefc8ef2a141c2022d7e7
Jeremy Vineyard <coinbump@gmail.com>
2022-04-27 00:32:15 -0700

    Simple Scan Sensor example scene

---
+++ PhaseJumpPro/Assets/phasejumppro/Templates/Any2D/ScreenTransformLimiter2D.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace PJ
+{
+    /// <summary>
+    /// Limits the transform to the visible orthographic bounds of the screen
+    /// Useful for fullscreen games, with no exterior UI
+    /// </summary>
+    // LIMITATIONS: this doesn't take into account pillar box or letterbox bars
+    public class ScreenTransformLimiter2D : SomeRectTransformLimiter2D
+    {
+        protected Optional<Vector2> size;
+            
+        public override Vector2 Size
+        {
+            get
+            {
+                if (null != size)
+                {
+                    return size.value;
+                }
+
+                var screenWorldSize = Utils.ScreenWorldSize();
+                
+                //Debug.Log("Limiter X Size: " + screenWorldSize.ToString() + " ySize: " + screenWorldSize.ToString());
+                
+                size = new Optional<Vector2>(screenWorldSize);
+                return size.value;
+            }
+        }
+    }
+}
