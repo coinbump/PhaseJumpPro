82175281242c960a2b16579db2c95dcab5f5a783
Jeremy Vineyard <coinbump@gmail.com>
2022-04-28 23:16:11 -0700

    Fix Breakout example from auto applying the layout and making the bricks not break

---
+++ PhaseJumpPro/Assets/Examples/Breakout Example/BKEBoard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PJ;
+
+public class BKEBoard : PJ.MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SomeGameObjectsLayout>().ApplyLayout();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
