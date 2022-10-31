e8a933820bc491fa73fefc5025ad0745f706fca8
Jeremy Vineyard <coinbump@gmail.com>
2022-10-31 05:36:37 -0700

    New SegmentStateTimer

---
+++ PhaseJumpPro/Assets/phasejumppro/Model/Time/SegmentStateTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * RATING: 5 stars
+ * Simple wrapper class
+ * CODE REVIEW: 6/22/22
+ */
+namespace PJ
+{
+	/// <summary>
+	/// A timer that sets a state when it enters a segment (a slice of time within the time size)
+	/// Example: a day-state timer for morning, afternoon, night
+	/// </summary>
+	public class SegmentStateTimer<State> : Timer
+    {
+        public SegmentState<State> segmentState;
+
+		public SegmentStateTimer(State initialState, float duration, SomeTimed.RunType type = SomeTimed.RunType.KeepRunning)
+			: base(duration, type)
+		{
+            segmentState = new SegmentState<State>(initialState);
+        }
+
+        public override void OnUpdate(TimeSlice time)
+        {
+            base.OnUpdate(time);
+
+            segmentState.NormalValue = Progress;
+        }
+    }
+}
