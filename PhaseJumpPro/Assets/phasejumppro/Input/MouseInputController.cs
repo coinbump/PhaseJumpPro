be499524d570a8984b592fd9f3f24311db45d27b
Jeremy Vineyard <coinbump@gmail.com>
2022-10-31 05:46:06 -0700

    Update MouseInputController.cs

--- PhaseJumpPro/Assets/phasejumppro/Input/MouseInputController.cs
+++ PhaseJumpPro/Assets/phasejumppro/Input/MouseInputController.cs
@@ -1,49 +1,44 @@
 ﻿using UnityEngine;
-using System;
-
-#if ENABLE_INPUT_SYSTEM
 using UnityEngine.InputSystem;
-#endif
 
 /*
  * RATING: 5 stars
- * Tested with new Input System
- * CODE REVIEW: 4/23/22
+ * Tested and works
+ * CODE REVIEW: 5/9/22
  */
 namespace PJ
 {
-	/// <summary>
-	/// Records mouse position on update
-	/// </summary>
-	public class MouseInputController : SomeInputController
+    /// <summary>
+    /// Records mouse position on update
+    /// </summary>
+    public class MouseInputController : SomeInputController
 	{
-		public Vector3 position { get; protected set; }
+		/// <summary>
+        /// By default, main camera is used, or link to overlay camera if needed
+        /// </summary>
+		public Camera inputCamera;
+
+		public Vector3 screenPosition { get; protected set; }
 		public Vector3 worldPosition { get; protected set; }
 		public Ray ray { get; protected set; }
 
+		public Mouse mouse;
+
 		public virtual void OnUpdate(TimeSlice time)
 		{
-#if ENABLE_INPUT_SYSTEM
-			Vector3 position = Vector3.zero;
-			var mouse = Mouse.current;
-			if (null !=  mouse)
-            {
-				position = mouse.position.ReadValue();
-            }
-#else
-			position = Input.mousePosition;
-#endif
-			worldPosition = Camera.main.ScreenToWorldPoint(position);
+			mouse = Mouse.current;
+			if (null == mouse) { return; }
+
+			var position = mouse.position.ReadValue();
+
+			var camera = this.inputCamera != null ? this.inputCamera : Camera.main;
+			worldPosition = camera.ScreenToWorldPoint(position);
 			ray = Camera.main.ScreenPointToRay(position);
 		}
 
 		public override bool IsAvailable()
 		{
-#if ENABLE_INPUT_SYSTEM
 			return Mouse.current != null;
-#else
-			return Input.mousePresent;
-#endif
 		}
 	}
 }
