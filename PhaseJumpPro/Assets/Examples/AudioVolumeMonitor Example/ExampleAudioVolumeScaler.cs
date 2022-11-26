24f8cfa390fec4f7ae565416ebef3a2b982a22de
Jeremy Vineyard <coinbump@gmail.com>
2022-11-26 10:45:50 -0800

    AudioVolumeMonitor Example

---
+++ PhaseJumpPro/Assets/Examples/AudioVolumeMonitor Example/ExampleAudioVolumeScaler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PJ;
+
+public class ExampleAudioVolumeScaler : PJ.MonoBehaviour
+{
+    protected AudioVolumeMonitor audioVolumeMonitor;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        audioVolumeMonitor = GetComponent<AudioVolumeMonitor>();
+    }
+
+    public override void OnUpdate(TimeSlice time)
+    {
+        base.OnUpdate(time);
+
+        var scale = 1.0f + 2.0f * audioVolumeMonitor.Volume;
+
+        transform.localScale = new Vector3(scale, scale, 1.0f);
+    }
+}
