9c613cb87b401b191189273f48b2e58c570f13a9
Jeremy Vineyard <coinbump@gmail.com>
2025-07-06 15:46:07 -0700

    Create SomeUpdatable.swift

---
+++ Swift/PhaseJumpPro/Sources/PhaseJumpPro/Core/SomeUpdatable.swift
@@ -0,0 +1,26 @@
+import Foundation
+import PhaseJumpPro
+
+/*
+ RATING: 5 stars
+ Simple types
+ CODE REVIEW: 7/6/25
+ */
+
+/// Return type for a finishing updatable func
+enum FinishType {
+    /// Keep running the updatable
+    case `continue`
+
+    /// Complete the updatable
+    case finish
+}
+
+/// Lightweight updatable, with no stored properties
+protocol SomeUpdatable {
+    /// Handle time update event
+    func onUpdate(time: TimeSlice) -> FinishType
+
+    /// Allows for cleanup of old updatables
+    var isFinished: Bool { get set }
+}
