62e3bf81725d4305f3c273c75148379271afd4ce
Jeremy Vineyard <coinbump@gmail.com>
2025-07-07 09:06:09 -0700

    Updatable + tests

--- Swift/PhaseJumpPro/Sources/PhaseJumpPro/Core/Updatable.swift
+++ Swift/PhaseJumpPro/Sources/PhaseJumpPro/Core/Updatable.swift
@@ -1,48 +1,52 @@
-//import Foundation
-//
-///// An object that receives time update events
-///// Finish the updatable when it's finished so it can be removed
-//public struct Updatable: SomeUpdatable {
-//    /// If true, this updatable is finished running and will be removed
-//    public var isFinished: Bool {
-//        didSet {
-//            guard isFinished != newValue,
-//                  isFinished else { return }
-//            
-//            onFinishFunc?(self)
-//        }
-//    }
-//
-//    public typealias OnUpdateFunc = (Self, TimeSlice) -> FinishType
-//    public typealias Func = (Self) -> Void
-//    
-//    /// (Optional). Called for update events
-//    public var onUpdateFunc: OnUpdateFunc?
-//
-//    /// (Optional). Called when this finishes
-//    public var onFinishFunc: Func
-//
-//    public init(onUpdateFunc: OnUpdateFunc? = nil) {
-//        self.onUpdateFunc = onUpdateFunc
-//    }
-//
-//    /// Finishes the updatable. After calling this it will not receive any more time updates
-//    public func finish() {
-//        isFinished = true
-//    }
-//
-//    /// @return Updates and returns finish type
-//    public func update(time: TimeSlice) -> FinishType {
-//        onUpdate(time)
-//    }
-//
-//    /// Called for each time delta event
-//    public func onUpdate(time: TimeSlice) {
-//        guard !isFinished else { return .finish }
-//        
-//        let result = onUpdateFunc(*this, time)
-//        isFinished = result == .finish
-//
-//        return result
-//    }
-//}
+/*
+ RATING: 5 stars
+ Has unit tests
+ CODE REVIEW: 7/7/25
+ */
+
+/// An object that receives time update events
+/// Finish the updatable when it's finished so it can be removed
+public final class Updatable: SomeUpdatable {
+    public typealias OnUpdateFunc = (Updatable, TimeSlice) -> FinishType
+    public typealias Func = (Updatable) -> Void
+    
+    /// If true, this updatable is finished running and will be removed
+    public var isFinished: Bool = false {
+        didSet {
+            guard isFinished != oldValue,
+                  isFinished else { return }
+            
+            onFinishFunc?(self)
+        }
+    }
+    
+    /// (Optional). Called for update events
+    public var onUpdateFunc: OnUpdateFunc?
+
+    /// (Optional). Called when this finishes
+    public var onFinishFunc: Func?
+
+    public init(
+        onUpdateFunc: OnUpdateFunc? = nil,
+        onFinishFunc: Func? = nil
+    ) {
+        self.onUpdateFunc = onUpdateFunc
+        self.onFinishFunc = onFinishFunc
+    }
+
+    /// Finishes the updatable. After calling this it will not receive any more time updates
+    public func finish() {
+        isFinished = true
+    }
+
+    /// Called for each time delta event
+    @discardableResult
+    public func onUpdate(time: TimeSlice) -> FinishType {
+        guard !isFinished else { return .finish }
+        
+        let result = onUpdateFunc?(self, time) ?? .continue
+        isFinished = result == .finish
+
+        return result
+    }
+}
---
+++ Swift/PhaseJumpPro/Tests/PhaseJumpProTests/UpdatableTests.swift
@@ -0,0 +1,74 @@
+import Foundation
+import Testing
+@testable import PhaseJumpPro
+
+struct UpdatableTests {
+    class Counter {
+        var finishCount = 0
+        var didFinish = false
+        var timeValue = 0.0
+    }
+    
+    @Test
+    func isFinished() async throws {
+        let counter = Counter()
+        let sut = Updatable()
+        
+        sut.onFinishFunc = { _ in
+            counter.didFinish = true
+        }
+        sut.onUpdateFunc = { _, _ in
+            .finish
+        }
+        
+        #expect(false == sut.isFinished)
+        sut.onUpdate(time: .init(delta: 1))
+        #expect(sut.isFinished)
+        #expect(counter.didFinish)
+    }
+    
+    @Test
+    func finish() async throws {
+        let counter = Counter()
+        let sut = Updatable()
+        
+        sut.onFinishFunc = { _ in
+            counter.didFinish = true
+        }
+        #expect(false == sut.isFinished)
+        sut.finish()
+        #expect(sut.isFinished)
+        #expect(counter.didFinish)
+    }
+    
+    @Test
+    func update() async throws {
+        let counter = Counter()
+        let sut = Updatable()
+        
+        sut.onUpdateFunc = { _, time in
+            counter.timeValue += time.delta
+            return .continue
+        }
+        #expect(false == sut.isFinished)
+        
+        sut.onUpdate(time: .init(delta: 3))
+        #expect(3 == counter.timeValue)
+    }
+    
+    @Test
+    func updateAfterfinish() async throws {
+        let counter = Counter()
+        let sut = Updatable()
+        
+        sut.onUpdateFunc = { _, time in
+            counter.timeValue += time.delta
+            return .continue
+        }
+        sut.finish()
+        #expect(sut.isFinished)
+        
+        sut.onUpdate(time: .init(delta: 3))
+        #expect(0 == counter.timeValue)
+    }
+}
