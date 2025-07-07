08eb7fc7848df520ed3c77e955a39d427bd9f479
Jeremy Vineyard <coinbump@gmail.com>
2025-07-06 21:41:03 -0700

    Update Timer.swift

--- Swift/PhaseJumpPro/Sources/PhaseJumpPro/Time/Timer.swift
+++ Swift/PhaseJumpPro/Sources/PhaseJumpPro/Time/Timer.swift
@@ -1,101 +1,94 @@
-//import Foundation
-//
-//public actor Timer: SomeUpdatable {
-//    
-//    // TODO: finish
-//#if false
-//    /// Timer duration
-//    private var duration: Double;
-//    
-//    /// Time state
-//    private var state = 0.0;
-//    
-//    private Runner runner;
-//    
-//    public typealias Func = (Self) -> Void
-//    
-//    /// (Optional). Called when this finishes
-//    var onFinishFunc: Func
-//    
-//    init(_ duration: Double, runType: RunType) :
-//    duration(duration),
-//    runner(runType) {
-//        runner.onFinishFunc = [this](Runner& runner) { this->OnFinish(); };
-//        runner.onResetFunc = [this](Runner& runner) { this->OnReset(); };
-//    }
-//    
-//    // Copy properties, not compose funcs (storing captured this)
-//    init(This const& rhs) :
-//    duration(rhs.duration),
-//    runner(rhs.runner.runType) {
-//        runner.onFinishFunc = [this](Runner& runner) { this->OnFinish(); };
-//        runner.onResetFunc = [this](Runner& runner) { this->OnReset(); };
-//    }
-//    
-//    float State() const {
-//        return state;
-//    }
-//    
-//    func SetState(float value) {
-//        GUARD(state != value)
-//        state = std::clamp(value, 0.0f, duration);
-//        runner.SetIsFinished(state >= duration);
-//    }
-//    
-//    func Stop() {
-//        runner.runType = RunType::Once;
-//        runner.SetIsFinished(true);
-//    }
-//    
-//    func Reset() {
-//        runner.Reset();
-//    }
-//    
-//    func OnReset() {
-//        SetState(0);
-//    }
-//    
-//    float Progress() const {
-//        return std::clamp(state / duration, 0.0f, 1.0f);
-//    }
-//    
-//    func SetProgress(float progress) {
-//        progress = std::clamp(progress, 0.0f, 1.0f);
-//        SetState(progress * duration);
-//    }
-//    
-//    func OnFinish() {
-//        GUARD(onFinishFunc);
-//        (onFinishFunc)(*this);
-//    }
-//    
-//    // MARK: SomeUpdatable
-//    
-//    FinishType OnUpdate(TimeSlice time) override {
-//        if (IsFinished() || duration <= 0) {
-//            return FinishType::Finish;
-//        }
-//        
-//        auto delta = TimeDeltaFor(time);
-//        if (delta <= 0) {
-//            return FinishType::Continue;
-//        }
-//        
-//        // We can't break down large deltas here because events will get compressed
-//        // Example: an event happens every 3 seconds, we get a 9 second delta,
-//        // they should be 3 seconds apart but are 0 seconds apart instead
-//        SetState(state + delta);
-//        
-//        return IsFinished() ? FinishType::Finish : FinishType::Continue;
-//    }
-//    
-//    float TimeDeltaFor(TimeSlice time) {
-//        return time.delta;
-//    }
-//    
-//    /// Allows for cleanup of old updatables
-//    bool IsFinished() const override {
-//        return runner.IsFinished();
-//    }
-//    #endif
-//};
+import Foundation
+
+public func clamp<T>(_ value: T, minValue: T, maxValue: T) -> T where T : Comparable {
+    return min(max(value, minValue), maxValue)
+}
+
+public actor Timer: SomeUpdatable {
+    public typealias Func = (Timer) -> Void
+    
+    /// Timer duration
+    private var duration: Double
+    
+    /// Time state
+    public var state = 0.0 {
+        didSet {
+            guard state != oldValue else { return }
+            state = clamp(state, minValue: 0.0, maxValue: duration)
+            runner.isFinished = state >= duration
+        }
+    }
+    
+    private var runner: Runner
+    
+    /// (Optional). Called when this finishes
+    var onFinishFunc: Func?
+    
+    init(_ duration: Double, runType: RunType)  {
+        self.duration = duration
+        self.runner = .init(runType: runType)
+   
+        runner.onFinishFunc = { self.onFinish() }
+        runner.onResetFunc = { self.onReset() }
+    }
+    
+    func stop() {
+        runner.runType = .once
+        runner.isFinished = true
+    }
+    
+    func reset() {
+        runner.reset()
+    }
+    
+    func onReset() {
+        state = 0
+    }
+    
+    var progress: Double {
+        clamp(state / duration, minValue: 0.0, maxValue: 1.0)
+    }
+    
+    func setProgress(_ progress: Double) {
+        let progress = clamp(progress, minValue: 0.0, maxValue: 1.0)
+        state = self.progress * duration
+    }
+    
+    func onFinish() {
+        onFinishFunc?(self)
+    }
+    
+    // MARK: SomeUpdatable
+    
+    public func onUpdate(time: TimeSlice) -> FinishType {
+        if isFinished || duration <= 0 {
+            return .finish
+        }
+        
+        let delta = timeDelta(for: time)
+        if delta <= 0 {
+            return .continue
+        }
+        
+        // We can't break down large deltas here because events will get compressed
+        // Example: an event happens every 3 seconds, we get a 9 second delta,
+        // they should be 3 seconds apart but are 0 seconds apart instead
+        state = state + delta
+        
+        return isFinished ? .finish : .continue
+    }
+    
+    func timeDelta(for time: TimeSlice) -> Double {
+        time.delta
+    }
+    
+    /// Allows for cleanup of old updatables
+    public var isFinished: Bool {
+        get {
+            runner.isFinished
+        }
+        set {
+            runner.isFinished = newValue
+        }
+    }
+}
